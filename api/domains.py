2b8a7f81906496c53ef74891445153408c83131e
Victor Bravo <victorbravofi@gmail.com>
2019-09-08 00:08:06 -0300

    post custom-domain

--- api/domains.py
+++ api/domains.py
@@ -11,18 +11,41 @@ def obtener_uno(domain):
             result = dns.resolver.query(domain, dns.rdatatype.A)
             domains[domain] = []
         except:
-            abort(404)
+            return abort(404)
 
         for answer in result.response.answer:
             if answer.rdtype == dns.rdatatype.A:
                 for item in answer:
-                    info_domain = {
+                    rr = {
                         'domain': domain,
                         'ip': item.to_text(),
                         'custom': False
                     }
-                    domains[domain].append(info_domain)
+                    domains[domain].append(rr)
 
     first = domains[domain].pop(0)
     domains[domain].append(first)
     return first
+
+def crear(**kwargs):
+    body = kwargs.get('body')
+    domain = body.get('domain')
+    ip = body.get('ip')
+    if not domain or not ip:
+        return abort(400)
+
+    for registers in domains.values():
+        for rr in registers:
+            if rr['ip'] == ip:
+                return abort(400)
+
+    rr = {
+        'domain': domain,
+        'ip': ip,
+        'custom': True
+    }
+
+    if domain not in domains:
+        domains[domain] = []
+    domains[domain].append(rr)
+    return make_response(rr, 201)
