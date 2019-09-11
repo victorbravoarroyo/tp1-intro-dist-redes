876be462564650e9b01bedaea2a23b65866f1f8a
Emiliano Prediger <emprediger@teco.com.ar>
2019-09-11 15:03:08 -0300

    POST error body incompleto, PUT chequeo dominio en URL y body. Formato e info swagger

--- api/domains.py
+++ api/domains.py
@@ -1,6 +1,5 @@
 from flask import abort, make_response, request
 import dns.resolver
-import dns.rdatatype
 
 # Custom domains registrados
 # Contiene pares clave:valor del tipo 'domain'(string): 'ip' (string)
@@ -32,7 +31,6 @@ def obtener_uno(domain):
             'error': 'domain not found'
         }
         return make_response(error, 404)
-        # return abort(404)
 
     if domain not in indexes:
         indexes[domain] = 0
@@ -57,15 +55,18 @@ def crear(**kwargs):
     body = kwargs.get('body')
     domain = body.get('domain')
     ip = body.get('ip')
+
     if not domain or not ip:
-        return abort(400)
+        error = {
+            "error": "payload is invalid"
+        }
+        return make_response(error, 400)
 
     if domain in custom_domains:
         existingCustom = {
             "error": "custom domain already exists"
         }
         return make_response(existingCustom, 400)
-        # return abort(400)
 
     custom_domains[domain] = ip
     item = {
@@ -73,9 +74,10 @@ def crear(**kwargs):
         'ip': ip,
         'custom': True
     }
+
     return make_response(item, 201)
 
-def agregar(**kwargs):
+def editar(domain, **kwargs):
     """
     Esta funcion maneja el request PUT /api/domains/{domain}
 
@@ -83,17 +85,20 @@ def agregar(**kwargs):
     :return: 200 domain, 404 no existe domain, 400 mal formato
     """
     body = kwargs.get('body')
-    domain = body.get('domain')
+    body_domain = body.get('domain')
     ip = body.get('ip')
-    if not domain or not ip:
-        error = {
-        "error": "payload is invalid"
-        }
-        return make_response(error, 400)
-        # return abort(400)
 
     if domain not in custom_domains:
-        return abort(404)
+        error = {
+            "error": "domain not found"
+        }
+        return make_response(error, 404)
+
+    if domain != body_domain or not body_domain or not ip :
+        error = {
+            "error": "payload is invalid"
+        }
+        return make_response(error, 400)
 
     custom_domains[domain] = ip
     item = {
@@ -104,20 +109,32 @@ def agregar(**kwargs):
     return make_response(item, 200)
 
 def borrar(domain):
+    """
+    Maneja DELETE /custom-domains/{domain}
+
+    :domain   dominio que se quiere borrar
+    :return:  200 domain, 404 dominio no encontrado
+    """
     if domain not in custom_domains:
         rr = {
             'error': 'domain not found'
         }
-        return make_response(rr,404)
+        return make_response(rr, 404)
+
     rr = {
         'domain':domain,
     }
-    del custom_domains[domain]
-    return make_response(rr,200)
 
-def query_custom_domain():
+    del custom_domains[domain]
+
+    return make_response(rr, 200)
+
+def query_custom_domains():
     """
     Búsqueda de custom domains creados mediante parámetro 'q' en URL
+
+    :param q: string a buscar en dominios
+    :return: 200 custom_domain que satisfacen query
     """
     query = request.args.get('q')
     items = []
